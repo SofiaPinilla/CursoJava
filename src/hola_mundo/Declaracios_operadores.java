aa5b57f778194daea7c4e9190b4e2b8b170abba2
Sofía Pinilla <contact@sofiapinilla.com>
2020-06-30 12:22:30 +0200

    primeras declaraciones de operadores

---
+++ src/hola_mundo/Declaracios_operadores.java
@@ -0,0 +1,14 @@
+package hola_mundo;
+
+public class Declaracios_operadores {
+    public static void main(String[] args) {
+        double a=5;
+        double b;
+        b=7;
+
+        double c= b/a;
+        //c++;
+        c+=2;
+System.out.print(c);
+    }
+}
