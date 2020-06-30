47355cc0cdb2b2d4d804c5bf10dbaea20e94346f
Sofía Pinilla <contact@sofiapinilla.com>
2020-06-30 12:45:10 +0200

    calculos con math

---
+++ src/hola_mundo/Calculos_conMath.java
@@ -0,0 +1,8 @@
+package hola_mundo;
+
+public class Calculos_conMath {
+    public static void main(String[] args) {
+        double raiz= Math.sqrt(9);
+        System.out.print(raiz);
+    }
+}
--- src/hola_mundo/Declaracios_operadores.java
+++ src/hola_mundo/Declaracios_operadores.java
@@ -2,13 +2,24 @@ package hola_mundo;
 
 public class Declaracios_operadores {
     public static void main(String[] args) {
-        double a=5;
-        double b;
-        b=7;
+      //  double a=5;
+        //double b;
+        //b=7;
 
-        double c= b/a;
+        //double c= b/a;
         //c++;
-        c+=2;
-System.out.print(c);
+        //c+=2;
+
+       // final double pulgadas=2.54;
+      //  double cm=6;
+      //  double resultado= cm/pulgadas;
+
+//System.out.print("En " + cm + "cm hay " + resultado + " pulgadas");
+
+        int operador1,operador2,resultado;
+        operador1=8;
+        operador2=7;
+        resultado=operador1+operador2;
+        System.out.print(resultado);
     }
 }
